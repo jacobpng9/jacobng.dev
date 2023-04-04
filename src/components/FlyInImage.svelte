c3f1aa306afdd8b917f7b577aaee2e018891d355
jacobng24 <110934976+jacobng24@users.noreply.github.com>
2023-04-04 13:04:14 -0700

    Fix responsiveness

--- src/components/FlyInImage.svelte
+++ src/components/FlyInImage.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import bidoof from "../images/bidoof.jpg";
   import viewport from "./useViewportAction";
-  import { fly, slide } from "svelte/transition";
+  import { fly } from "svelte/transition";
   import { quintOut } from "svelte/easing";
   let show = false;
 </script>
@@ -21,20 +21,20 @@
     <div class="blurbcontainer">
       <section
         class="blurb"
-        style="font-size: 2rem"
+        style="font-size: 18px"
         in:fly={{ easing: quintOut, duration: 1000, delay: 400 }}
       >
-        <span class="text-l hubot" style="color: var(--jpurple)">Jacob Ng</span>
+        <span class=" text-l hubot" style="color: var(--jpurple)">About Me</span>
         <br />Hello! My name is Jacob Ng, a current high school junior at Brophy
         College Preparatory. Coding is one of my favorite pastimes, and I have
         been learning all sorts of languages like Java, Python, HTML, CSS, and
-        Javascript since Freshman year. I love music, video games, and fashion.
+        Javascript since Freshman year. <br><br>I love music, video games, and fashion.
         Enjoy the website and my portfolios!
       </section>
     </div>
     <div
       class="imagecontainer"
-      in:slide={{ axis: "y", easing: quintOut, duration: 1000 }}
+      in:fly={{ easing: quintOut, duration: 1000, delay: 400 }}
     >
       <img src={bidoof} alt="" class="aboutimage" />
     </div>
