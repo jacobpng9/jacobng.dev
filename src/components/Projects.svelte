8f619d1f7df393fe6cd695b6e9bc58e66ab4d4a4
jacobng24 <110934976+jacobng24@users.noreply.github.com>
2023-09-19 19:26:49 -0700

    new button in new section

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+  import { fly } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
   import FlyIn from "./FlyIn.svelte";
+  import Button from "./Button.svelte";
 </script>
 
 <FlyIn>
-  <div class="card" id="cardContainer">
-    <div class="card" id="project1" />
+  <div id="projects">
+    <div id="">
+    </div>
   </div>
+  <div in:fly={{ easing: quintOut, duration: 1000, delay: 200 }}>
+      <Button number={2} className={"margin"} />
+    </div>
 </FlyIn>
