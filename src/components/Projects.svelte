f710af5c86bc2cd97ed8de2a438e51769deb1770
jacobng24 <110934976+jacobng24@users.noreply.github.com>
2023-10-08 14:20:29 -0700

    fix overflow of cards; update node

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -9,32 +9,47 @@
 
 <FlyIn>
   <div id="projects">
-    <div id="title"><div id="projectsTitle" class="hubot text-l center">Projects</div></div>
+    <div id="title">
+      <div id="projectsTitle" class="hubot text-l center">Projects</div>
+    </div>
     <div class="grid-container">
       <div class="card">
         <div class="card_content">
-        <div class="imagecontaine">
-          <img src={Screen1} alt="haii^_^" class="card_img">
-        </div>
+          <div class="imagecontaine">
+            <img src={Screen1} alt="haii^_^" class="card_img" />
+          </div>
           <br />Lorem ipsum dolor sit amet,asdasdadasdasdadasdas
         </div>
-      </div>
-      <div class="card">
-        <div class="card_content">
-          <img src={Jacob} alt="haii^_^" class="card_img">
-
-          <span class="text-m hubot" style="color: var(--jpurple)">Personal Website</span>
-        Personal website to show off portfolios and projects. 
+        <div class="footer">
+          <div class="hubot text-m" style="color: var(--jpurple)">Github</div>
         </div>
       </div>
       <div class="card">
         <div class="card_content">
-           <div class="imagecontaine"><img src={Jacob} alt="haii^_^" class="card_img"></div>
+          <img src={Jacob} alt="haii^_^" class="card_img" />
+
+          <span class="text-m hubot" style="color: var(--jpurple)"
+            >Personal Website</span
+          >
+          Personal website to show off portfolios and projects.
+        </div>
+        <div class="footer">
+          <div class="hubot text-m" style="color: var(--jpurple)">Github</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card_content">
+          <div class="imagecontaine">
+            <img src={Jacob} alt="haii^_^" class="card_img" />
+          </div>
           <br />Hello! My name is Jacob Ng, a current high school junior at
           Brophy College Preparatory. I have been learning all sorts of
           languages like Java, Python, HTML, CSS, and Javascript since Freshman
           year. <br /><br />I love music, video games, and fashion. Enjoy the
           website and my portfolios!
+        </div>
+        <div class="footer">
+          <div class="hubot text-m" style="color: var(--jpurple)">Github</div>
         </div>
       </div>
     </div>
