839ca2b4b1d4e342713053c24fb546062664d827
jacobng24 <110934976+jacobng24@users.noreply.github.com>
2023-11-07 11:32:58 -0700

    Fix navbuttons; button hover cursor

---
+++ src/components/NavButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  const buttons = ["Home", "About", "Projects", "Photos", "Contact"];
+</script>
+
+{#each buttons as b, number}
+  <button class="head resetButton"
+    on:click={() =>
+      window.scroll({
+        top: window.innerHeight * number,
+        behavior: "smooth",
+      })}
+  >
+    {b}
+  </button>
+{/each}
