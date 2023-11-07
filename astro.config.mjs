7b16fc149997c8a41b14f4e465ee2ad06d81873a
jacobng24 <110934976+jacobng24@users.noreply.github.com>
2023-11-07 13:49:40 -0700

    men

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), solidJs()],
 });
---
+++ src/components/TextBackground.tsx
@@ -0,0 +1,78 @@
+import { For, createSignal, from } from "solid-js";
+
+const LINE_HEIGHT = 20;
+const LETTER_WIDTH = 9;
+const ALL_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+
+export default function TextBackground() {
+   const [height, setHeight] = createSignal(window.outerHeight);
+   const [width, setWidth] = createSignal(window.outerWidth);
+
+   const letters = () => calculateLetterAmounts(height(), width()).letters;
+   const lines = () => calculateLetterAmounts(height(), width()).lines;
+
+   const text = from<string[][]>((set) => {
+      const interval = setInterval(() => {
+         set((prev) => {
+            if (prev !== undefined) {
+               const a = prev.shift()!;
+               prev.push(a);
+               return prev;
+            }
+
+            prev = [];
+
+            for (let i = 0; i < lines(); i++) {
+               const newPart = Array.from(
+                  { length: letters() },
+                  () => ALL_LETTERS[Math.floor(Math.random() * ALL_LETTERS.length)]
+               ) as string[];
+               prev.push(newPart);
+            }
+
+            return prev;
+         });
+      }, 100);
+
+      function handleResize() {
+         set(undefined);
+         setHeight(window.innerHeight);
+         setWidth(window.innerWidth);
+      }
+
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+         clearInterval(interval);
+         window.removeEventListener("resize", handleResize);
+      };
+   });
+
+   return (
+      <div
+         class="overflow-hidden max-w-full max-h-full font-mono whitespace-pre bg-gradient-to-t select-none gradient-text bg-gradient-to-indigo-800 bg-gradient-from-sky-800"
+         style={{
+            "line-height": "20px",
+         }}
+         aria-hidden={true}
+      >
+         <For each={text()}>
+            {(line) => {
+               return (
+                  <>
+                     {line.join("")}
+                     <br />
+                  </>
+               );
+            }}
+         </For>
+      </div>
+   );
+}
+
+function calculateLetterAmounts(height: number, width: number): { lines: number; letters: number } {
+   const lines = Math.floor(height / LINE_HEIGHT);
+   const letters = Math.ceil(width / LETTER_WIDTH);
+
+   return { lines, letters };
+}
